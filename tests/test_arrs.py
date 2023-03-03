b88d6b81b68928a86c1b6bfb657ad5b0ae672f55
Mkupr <mkupr@outlook.com>
2023-03-03 20:41:58 +0300

    modify test_arrs.py by using textures

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,14 +2,22 @@ import pytest
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], 0, "test") == "test"
+@pytest.fixture
+def test_data():
+    # return [[], [1, 2, 3], [1, 2, 3, 4]]
+    return [], [1, 2, 3], [1, 2, 3, 4]
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], -4) == [1, 2, 3]
-    assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
-    assert arrs.my_slice([], 1) == []
+def test_get(test_data):
+    list_0, list_1, list_2 = test_data
+    assert arrs.get(list_1, 1, "test") == 2
+    assert arrs.get(list_0, 0, "test") == "test"
+
+
+def test_slice(test_data):
+    list_0, list_1, list_2 = test_data
+    assert arrs.my_slice(list_2, 1, 3) == [2, 3]
+    assert arrs.my_slice(list_1, 1) == [2, 3]
+    assert arrs.my_slice(list_1, -4) == [1, 2, 3]
+    assert arrs.my_slice(list_1, -2) == [2, 3]
+    assert arrs.my_slice(list_0, 1) == []
